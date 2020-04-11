372d7c66464dbc6f387a97e10d8bfc0b90f0df50
Жидких Иван <zhidkikh.i.o@sberbank.ru>
2020-04-11 22:21:16 +0300

    Подготовка демо

--- src/python/demo_service/demo_service/demo_service.py
+++ src/python/demo_service/demo_service/demo_service.py
@@ -2,13 +2,12 @@
 
 if __name__ == '__main__':
     import time
-    import systemd.daemon
 
     print('Starting up ...')
     time.sleep(10)
     print('Startup complete')
     # Tell systemd that our service is ready
-    systemd.daemon.notify('READY=1')
+    #systemd.daemon.notify('READY=1')
 
     while True:
         print('Hello from the Python Demo Service')
